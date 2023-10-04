e0351a92d1906ea268e88713611a741e362dde66
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-10-04 12:24:56 +0200

    yet another quality commit

--- js/index.js
+++ js/index.js
@@ -18,31 +18,75 @@ function getCurrentHour() {
     return currentHour - 12 + "pm";
   }
 }
+// ------------ solution 1 -------------
+const getCurrentHour = () => {
+  const now = new Date();
+  const currentHour = now.getHours();
+  if (currentHour === 0) {
+    return "12am";
+  } else if (currentHour === 12) {
+    return "12pm";
+  } else if (currentHour <= 11) {
+    return currentHour + "am";
+  } else {
+    return currentHour - 12 + "pm";
+  }
+}
+//----------------------------------------------------------------------
 
 function getVectorLength(x, y, z) {
   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
 }
+// ------------ solution 2 -------------
+const getVectorLength = (x, y, z) => (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+//------------------------------------------------------------------------
 
 function cleanInput(string) {
   return string.toLowerCase().trim();
 }
+// ------------ solution 3 -------------
+const cleanInput = string => string.toLowerCase().trim();
+// ----------------------------------
 
-/*
-Rewrite the following arrow functions as classic functions.
+/*Rewrite the following arrow functions as classic functions.
 */
 
 const isOddAndSmall = (number) => {
   if (number > 10) {
     return false;
   }
-
   if (number % 2 === 0) {
     return false;
   }
-
+  
   return true;
 };
 
+// ------------ solution 4 -------------
+function isOddAndSmall (number){
+  if (number > 10) {
+    return false;
+  }
+  if (number % 2 === 0) {
+    return false;
+  }
+  
+  return true;
+}
+//----------------------------------------------------------
+
 const add3 = (a, b, c) => a + b + c;
 
+// ------------ solution 5 -------------
+function add3 (a, b, c){
+  return a + b + c
+}
+//------------------------------------------------------------
+
 const repeat10 = (string) => string.repeat(10);
+
+// ------------ solution 6 -------------
+
+function repeat10 (string) {
+  return string.repeat(10)
+}
